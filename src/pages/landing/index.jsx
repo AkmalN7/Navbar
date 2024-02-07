38b806ef247be33e61ef95fcd8a6aa039cb9461d
AkmalN7 <akmalnasriddinov15@gmail.com>
2024-02-07 18:03:21 +0500

    qwer

---
+++ src/pages/landing/index.jsx
@@ -0,0 +1,202 @@
+import React from 'react'
+import { Fade, Zoom, Flip } from 'react-reveal'
+
+export default function index() {
+return (
+    <div className='row'>
+        <div className="col-md-6 my-3">
+        <Fade left>
+        <div className="card">
+                <div className="card-header">
+                    <h1>Lorem, ipsum.</h1>
+                </div>
+                <div className="card-body">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam asperiores quod voluptatem ad, ratione incidunt at iusto sunt saepe provident!
+                    </p>
+                </div>
+                <div className="card-footer">
+                    <button className='btn btn-primary'> Subscribe </button>
+                </div>
+            </div>
+        </Fade>
+        </div>
+        <div className="col-md-6 my-3">
+            <Fade right>
+            <div className="card">
+                <div className="card-header">
+                    <h1>Lorem, ipsum.</h1>
+                </div>
+                <div className="card-body">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam asperiores quod voluptatem ad, ratione incidunt at iusto sunt saepe provident!
+                    </p>
+                </div>
+                <div className="card-footer">
+                    <button className='btn btn-success'> Subscribe </button>
+                </div>
+            </div>
+            </Fade>
+        </div>
+        <div className="col-md-6 my-3">
+            <Fade top>
+            <div className="card">
+                <div className="card-header">
+                    <h1>Lorem, ipsum.</h1>
+                </div>
+                <div className="card-body">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam asperiores quod voluptatem ad, ratione incidunt at iusto sunt saepe provident!
+                    </p>
+                </div>
+                <div className="card-footer">
+                    <button className='btn btn-success'> Subscribe </button>
+                </div>
+            </div>
+            </Fade>
+        </div>
+        <div className="col-md-6 my-3">
+            <Fade bottom>
+            <div className="card">
+                <div className="card-header">
+                    <h1>Lorem, ipsum.</h1>
+                </div>
+                <div className="card-body">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam asperiores quod voluptatem ad, ratione incidunt at iusto sunt saepe provident!
+                    </p>
+                </div>
+                <div className="card-footer">
+                    <button className='btn btn-success'> Subscribe </button>
+                </div>
+            </div>
+            </Fade>
+        </div>
+
+        <div className="row">
+        <div className="col-md-6 my-3">
+            <div className="card">
+                <div className="card-header">
+                    <h1>Lorem, ipsum.</h1>
+                </div>
+                <div className="card-body">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam asperiores quod voluptatem ad, ratione incidunt at iusto sunt saepe provident!
+                    </p>
+                </div>
+                <div className="card-footer">
+                    <button className='btn btn-primary'> Subscribe </button>
+                </div>
+            </div>
+        </div>
+        <div className="col-md-6 my-3">
+            <div className="card">
+                <div className="card-header">
+                    <h1>Lorem, ipsum.</h1>
+                </div>
+                <div className="card-body">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam asperiores quod voluptatem ad, ratione incidunt at iusto sunt saepe provident!
+                    </p>
+                </div>
+                <div className="card-footer">
+                    <button className='btn btn-success'> Subscribe </button>
+                </div>
+            </div>
+        </div>
+        <div className="col-md-6 my-3">
+            <div className="card">
+                <div className="card-header">
+                    <h1>Lorem, ipsum.</h1>
+                </div>
+                <div className="card-body">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam asperiores quod voluptatem ad, ratione incidunt at iusto sunt saepe provident!
+                    </p>
+                </div>
+                <div className="card-footer">
+                    <button className='btn btn-success'> Subscribe </button>
+                </div>
+            </div>
+        </div>
+        <div className="col-md-6 my-3">
+            <div className="card">
+                <div className="card-header">
+                    <h1>Lorem, ipsum.</h1>
+                </div>
+                <div className="card-body">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam asperiores quod voluptatem ad, ratione incidunt at iusto sunt saepe provident!
+                    </p>
+                </div>
+                <div className="card-footer">
+                    <button className='btn btn-success'> Subscribe </button>
+                </div>
+            </div>
+        </div>
+        </div>
+        <div className="row">
+        <div className="col-md-6 my-3">
+            <div className="card">
+                <div className="card-header">
+                    <h1>Lorem, ipsum.</h1>
+                </div>
+                <div className="card-body">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam asperiores quod voluptatem ad, ratione incidunt at iusto sunt saepe provident!
+                    </p>
+                </div>
+                <div className="card-footer">
+                    <button className='btn btn-primary'> Subscribe </button>
+                </div>
+            </div>
+        </div>
+        <div className="col-md-6 my-3">
+            <div className="card">
+                <div className="card-header">
+                    <h1>Lorem, ipsum.</h1>
+                </div>
+                <div className="card-body">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam asperiores quod voluptatem ad, ratione incidunt at iusto sunt saepe provident!
+                    </p>
+                </div>
+                <div className="card-footer">
+                    <button className='btn btn-success'> Subscribe </button>
+                </div>
+            </div>
+        </div>
+        <div className="col-md-6 my-3">
+            <div className="card">
+                <div className="card-header">
+                    <h1>Lorem, ipsum.</h1>
+                </div>
+                <div className="card-body">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam asperiores quod voluptatem ad, ratione incidunt at iusto sunt saepe provident!
+                    </p>
+                </div>
+                <div className="card-footer">
+                    <button className='btn btn-success'> Subscribe </button>
+                </div>
+            </div>
+        </div>
+        <div className="col-md-6 my-3">
+            <div className="card">
+                <div className="card-header">
+                    <h1>Lorem, ipsum.</h1>
+                </div>
+                <div className="card-body">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam asperiores quod voluptatem ad, ratione incidunt at iusto sunt saepe provident!
+                    </p>
+                </div>
+                <div className="card-footer">
+                    <button className='btn btn-success'> Subscribe </button>
+                </div>
+            </div>
+        </div>
+        </div>
+    </div>
+)
+}
